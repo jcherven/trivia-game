9b31433606dfa834fa1131791729f280e82008c8
James Cherven <jcherven@mail.usf.edu>
2018-09-17 14:32:23 -0400

    Adds object-oriented game init; Adds timer; Adds question text display; Adds some time up handling for answer buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,65 @@
                 / /_/ / /_/ / / / / / /  __/
                 \____/\__,_/_/ /_/ /_/\___/ 
 */
+
+window.onload = function() {
+  $('#start-button').on('click', activeQuestion.start);
+  $('.answer-button').on('click', activeQuestion.stop);
+}
+
+const timerLimit = 10;
+var intervalId;
+var timeValue = timerLimit;
+var timerRunning = false;
+var questionNumber = 0
+var activeQuestionText = 'What\'s it called when u talk about someone in a tweet without @ing their handle???';
+
+// Question-Answer Handling Object
+var activeQuestion = {
+  time: timerLimit,
+  questionNumber: this.questionNumber,
+
+  reset: function() {
+    activeQuestion.time = timerLimit;
+    this.questionNumber = 0;
+
+    $('#timer-digital-display').text(this.timerLimit);
+
+    $('question-number-display').text(activeQuestion.questionNumber);
+  },
+
+  start: function() {
+    if (!timerRunning) {
+      activeQuestion.questionNumber++;
+      $('#start-button').replaceWith('<div class="p-2">' + activeQuestionText + '</div>');
+      activeQuestion.displayQuestionNum();
+      intervalId = setInterval(activeQuestion.eachCount, 1000);
+      timerRunning = true;
+    }
+    
+  },
+
+  stop: function() {
+    clearInterval(intervalId);
+    timerRunning = false;
+    
+  },
+
+  eachCount: function() {
+    activeQuestion.time--
+    $('#timer-digital-display').text(activeQuestion.time);
+    if (timerRunning) {
+      if ( activeQuestion.time <= 0) {
+        activeQuestion.stop();
+        $('.answer-button').addClass('disabled');
+      }
+    }
+  },
+
+  displayQuestionNum: function() {
+    $('#question-number-display').text(activeQuestion.questionNumber);
+  }
+}
+
+
+// Timer Object
