90ac192044bee3b0f91d023dde42a0a1096e418f
James Cherven <jcherven@mail.usf.edu>
2018-09-18 13:12:45 -0400

    Gets the timer progress bar working, but it's sloppy due to JS concurrency issues (i.e. I don't know what I'm doing).

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,22 @@
-/*
-  ______     _       _                      
- /_  __/____(_)   __(_)___ _                
-  / / / ___/ / | / / / __ `/                
- / / / /  / /| |/ / / /_/ /                 
-/_/ /_/  /_/ |___/_/\__,_/                  
-                   ______                   
-                  / ____/___ _____ ___  ___ 
-                 / / __/ __ `/ __ `__ \/ _ \
-                / /_/ / /_/ / / / / / /  __/
-                \____/\__,_/_/ /_/ /_/\___/ 
-*/
+/**
+*   ______     _       _                      
+*  /_  __/____(_)   __(_)___ _                
+*   / / / ___/ / | / / / __ `/                
+*  / / / /  / /| |/ / / /_/ /                 
+* /_/ /_/  /_/ |___/_/\__,_/                  
+*                    ______                   
+*                   / ____/___ _____ ___  ___ 
+*                  / / __/ __ `/ __ `__ \/ _ \
+*                 / /_/ / /_/ / / / / / /  __/
+*                 \____/\__,_/_/ /_/ /_/\___/ 
+**/
 
 window.onload = function() {
   $('#start-button').on('click', activeQuestion.timerStart);
   $('.answer-button').on('click', activeQuestion.timerStop);
 }
 
-const timerLimit = 10;
+var timerLimit = 20;
 var intervalId;
 var timeValue = timerLimit;
 var timerRunning = false;
@@ -27,6 +27,7 @@ var activeQuestionText = 'What\'s it called when u talk about someone in a tweet
 var activeQuestion = {
   time: timerLimit,
   questionNumber: this.questionNumber,
+  timerProgressValue: timerLimit - 1,
 
   reset: function() {
     activeQuestion.time = timerLimit;
@@ -55,13 +56,25 @@ var activeQuestion = {
   },
 
   eachCount: function() {
-    activeQuestion.time--
+    activeQuestion.time--;
+    activeQuestion.timerProgressValue--;
+    // Update the timer progress bar
+    $('#timer-progress').attr('style', 'width: ' + convertRange(activeQuestion.timerProgressValue, [0, timerLimit]) + '%');
+    // Check if time has expired
     $('#timer-digital-display').text(activeQuestion.time);
     if (timerRunning) {
       if ( activeQuestion.time <= 0) {
         activeQuestion.timerStop();
         $('.answer-button').addClass('disabled');
       }
+    }
+
+    /** convertRange() maps an arbitrary range to 0-100, for calculating the progress bar value. Accepts range as an array of [min, max].
+     * The generalized formula used is:
+     * ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
+     */
+    function convertRange(value, range) {
+      return ( value - range[ 0 ] ) * ( 100 ) / ( range[ 1 ] - range[ 0 ] );
     }
   },
 
